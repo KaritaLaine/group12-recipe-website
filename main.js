eb01a3c4a52adf44e95bffb1113b94d7cc66d821
KaritaLaine <44746@edu.raseko.fi>
2025-11-24 17:05:56 +0200

    feat: render recipes as home page

---
+++ main.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv"
+import express from "express"
+import layouts from "express-ejs-layouts"
+
+dotenv.config()
+
+const app = express()
+
+// Get port number from the .env-file or default to 3000
+const port = process.env.PORT || 3000
+
+app.set("view engine", "ejs")
+app.set("port", port)
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.static("public"))
+
+app.use(layouts)
+
+app.get("/", (req, res) => {
+  res.render("recipes/index", { layout: "components/layout" })
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
