133bdadcbbc5b1ff1c740c4f9242c7bb33a20978
Eemeli <juho.e.nissinen@edu.turkuamk.fi>
2025-11-27 14:59:44 +0200

    feat: add route for creating a user

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv"
 import express from "express"
 import layouts from "express-ejs-layouts"
 import mongoose from "mongoose"
+import { usersController } from "./controllers/usersController"
 
 dotenv.config()
 
@@ -48,6 +49,8 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
   res.render("users/register")
 })
+
+app.post("/users/create", usersController.create, usersController.redirectView)
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
