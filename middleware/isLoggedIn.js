b2e6e2ab22335662be9619790046a17e5726448e
KaritaLaine <44746@edu.raseko.fi>
2025-12-01 17:22:38 +0200

    feat: create a middleware for redirecting users if there shouldn't be access

---
+++ middleware/isLoggedIn.js
@@ -0,0 +1,13 @@
+export const isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect("/login")
+}
+
+export const isNotLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect("/")
+}
